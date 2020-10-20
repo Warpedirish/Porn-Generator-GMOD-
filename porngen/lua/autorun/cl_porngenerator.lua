61673727deb08316b4c237c1feb0152d8cc5b6f0
Warpedirish <55043249+Warpedirish@users.noreply.github.com>
2020-10-20 15:30:54 -0700

    Update cl_porngenerator.lua

--- porngen/lua/autorun/cl_porngenerator.lua
+++ porngen/lua/autorun/cl_porngenerator.lua
@@ -4,6 +4,9 @@ net.Receive("initMenu", function()
   pf:Center()
   pf:SetTitle("Porn Generator")
   pf:MakePopup()
+  pf.Paint = function(self,w,h)
+    draw.RoundedBox(0,0,0,w,h,Color(20,20,20))
+  end
 
   local pornp = vgui.Create("DFrame",pf)
   pornp:Dock(FILL)
@@ -22,6 +25,13 @@ net.Receive("initMenu", function()
   rfshbtn:SetPos(15,25)
   rfshbtn:SetText("Refresh Porn")
   rfshbtn.DoClick = function() -- refresh
+  rfshbtn.Paint = function(self,w,h)
+    self:SetTextColor(Color(255,255,255))
+    draw.RoundedBox(0,0,0,w,h,Color(35,35,35))
+
+    surface.SetDrawColor(0,0,0,255)
+    surface.DrawOutlinedRect(0,0,self:GetWide(),self:GetTall())
+  end
 
   porn = vgui.Create("DHTML", pornp)
   porn:Dock(FILL)
